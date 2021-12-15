626ca8056967d1df14d0ecc16516e26b33a3e5e5
Steve Lim <stevelim3841@gmail.com>
2021-12-15 11:47:56 +0000

    init

---
+++ Hellow_word.cpp
@@ -0,0 +1,13 @@
+/*write a C++ program that outputs the following statement:
+*** "Hello world, I am ready for C++"
+*/
+
+#include <iostream>
+
+int main() {
+    
+    // print out hello world
+    std::cout<<"Hello world, I am ready for C++";
+    
+    return 0;
+}
