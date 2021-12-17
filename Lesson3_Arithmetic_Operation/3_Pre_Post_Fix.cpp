b5f0ac9c26587f97a008100820d370c556a629fc
Steve Lim <stevelim3841@gmail.com>
2021-12-17 19:50:48 +0000

    Lec 4

---
+++ Lesson3_Arithmetic_Operation/3_Pre_Post_Fix.cpp
@@ -0,0 +1,23 @@
+/*Goal: Practice PreFix and PostFix
+**
+*/
+
+// http://en.cppreference.com/w/cpp/language/operator_incdec
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b = 0;
+    int post, pre = 0;
+    cout<<"Inital values: \t\t\tpost = "<<post<<" pre= "<<pre<<"\n";
+    post = a++;
+    pre = ++b;
+    cout<<"After one postfix and prefix: \tpost = "<<post<<" pre= "<<pre<<"\n";
+    post = a++;
+    pre = ++b;
+    cout<<"After two postfix and prefix: \tpost = "<<post<<" pre= "<<pre<<"\n";  
+    return 0;
+}
---
+++ Lesson4_Control_Flow/5_Switch_Statement.cpp
@@ -0,0 +1,75 @@
+/*Goal: demonstrate use cases for the switch statement.*/
+
+#include <iostream>
+
+int main()
+{
+    int menuItem = 1;
+    
+    std::cout<<"What is your favorite winter sport?: \n";
+    std::cout<<"1.Skiing\n2: Sledding\n3: Sitting by the fire";
+    std::cout<<"\n4.Drinking hot chocolate\n";
+    std::cout<<"\n\n";
+    
+    switch(menuItem)
+    {
+        case(1): std::cout<<"Skiing?! Sounds dangerous!\n";
+                 break;
+        case(2): std::cout<<"Sledding?! Sounds like work!\n";
+                 break; 
+        case(3): std::cout<<"Sitting by the fire?! Sounds warm!\n";
+                 break;
+        case(4): std::cout<<"Hot chocolate?! Yum!\n";
+                 break;
+        default: std::cout<<"Enter a valid menu item";
+    }
+    
+    char begin;
+    std::cout<<"\n\nWhere do you want to begin?\n";
+    std::cout<<"B. At the beginning?\nM. At the middle?";
+    std::cout<<"\nE. At the end?\n\n";
+    begin = 'M';  
+    
+    switch(begin)
+    {
+        // no breaks, so continue below case statements
+        case('B'): std::cout<<"Once upon a time there was a wolf.\n";
+        case('M'): std::cout<<"The wolf hurt his leg.\n";
+        case('E'): std::cout<<"The wolf lived happily everafter\n";
+    }
+
+    float in1, in2;
+    char operation;
+    float answer;
+
+    std::cout<<"Enter two numbers:\n";
+    std::cin>>in1;
+    std::cin>>in2;
+    std::cout<<"Enter the operation '+','-','*','/':\n";
+    std::cin>>operation;
+
+    switch(operation)
+    {
+        case('+'):  {
+                    answer=in1 + in2;
+                    break;
+                    }
+         case('-'):  {
+                    answer=in1 - in2;
+                    break;
+                    }  
+        case('*'):  {
+                    answer=in1 * in2;
+                    break;
+                    }    
+        case('/'):  {
+                    answer=in1 / in2;
+                    break;
+                    }  
+        default:
+                    std::cout<<"Illegal operation";
+    }
+
+    std::cout<<in1<<operation<<in2<<" = "<<answer<<"\n";
+    return 0;
+}
